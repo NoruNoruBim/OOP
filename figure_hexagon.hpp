69521b16e752c137b4cef39b680aee6571599d6b
Anton <baranoff.barton@yandex.ru>
2017-09-25 04:22:32 +0300

    Update figure_hexagon.hpp

--- figure_hexagon.hpp
+++ figure_hexagon.hpp
@@ -2,11 +2,14 @@
 #define HEXAGON_H
 
 #include <cstdlib>
+#include <iostream>
 #include "figure.hpp"
 
 class Hexagon : public Figure{
     public:
         Hexagon ();
+        Hexagon(std::istream &is);
+        Hexagon(const Hexagon& orig);
 
         double Square() override;
         void Print() override;
