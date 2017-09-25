6a6ffdcc53d9bf9672b5611b6809ebe74840515e
Anton <baranoff.barton@yandex.ru>
2017-09-25 04:24:57 +0300

    Update figure_pentagon.hpp

--- figure_pentagon.hpp
+++ figure_pentagon.hpp
@@ -2,12 +2,15 @@
 #define PENTAGON_H
 
 #include <cstdlib>
+#include <iostream>
 #include "figure.hpp"
 
 class Pentagon : public Figure{
     public:
         Pentagon();
-    
+        Pentagon(std::istream &is);
+        Pentagon(const Pentagon& orig);
+
         double Square() override;
         void Print() override;
 
