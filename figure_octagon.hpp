0e1ad9a7067d07b625f22af372e1b4bbf2d26365
Anton <baranoff.barton@yandex.ru>
2017-09-25 04:24:02 +0300

    Update figure_octagon.hpp

--- figure_octagon.hpp
+++ figure_octagon.hpp
@@ -2,11 +2,14 @@
 #define OCTAGON_H
 
 #include <cstdlib>
+#include <iostream>
 #include "figure.hpp"
 
 class Octagon : public Figure{
     public:
         Octagon();
+        Octagon(std::istream &is);
+        Octagon(const Octagon& orig);
 
         double Square() override;
         void Print() override;
